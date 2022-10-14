3bd82065ec7f8f253bcb18832b200dbf07707a3b
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-10-14 16:06:56 +0100

    Remove instance for Option

--- src/Flat/Instances/Base.hs
+++ src/Flat/Instances/Base.hs
@@ -129,12 +129,6 @@ instance Flat a => Flat (Semigroup.Last a) where
     encode (Semigroup.Last a) = encode a
     size (Semigroup.Last a) = size a
     decode = Semigroup.Last <$> decode
-
--- | @since 0.4.4
-instance Flat a => Flat (Semigroup.Option a) where
-    encode (Semigroup.Option a) = encode a
-    size (Semigroup.Option a) = size a
-    decode = Semigroup.Option <$> decode
 #endif
 
 {- |
